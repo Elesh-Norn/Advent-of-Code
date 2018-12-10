3b54c94783e6a5e61d1331c4c91366a4313f27ea
Elesh-Norn <kercyan@live.fr>
2018-12-10 23:45:18 +0100

    Day 9 part 1

--- Day 9/Day_9.py
+++ Day 9/Day_9.py
@@ -1,18 +1,21 @@
+import numpy as np
+
 def game(player, last_marble):
     # Set the game
-    global STACK, CURRENT_MARBLE, player_list, ACTIVE_PLAYER, SCORE, BEST_MARBLE
+    global STACK, CURRENT_MARBLE, player_list, ACTIVE_PLAYER, SCORE
     STACK = [0]
     CURRENT_MARBLE = 1
     player_list = [x for x in range(1, player +1)]
     ACTIVE_PLAYER = 1
     SCORE = {}
-    BEST_MARBLE = 0
+
     for player in player_list:
         SCORE[player] = 0
 
-    while last_marble > BEST_MARBLE:
+    while last_marble > CURRENT_MARBLE:
         turn()
-
+        if CURRENT_MARBLE % 71700 == 0:
+            print(CURRENT_MARBLE/71700)
     return SCORE
 
 def player_change():
@@ -20,31 +23,28 @@ def player_change():
     ACTIVE_PLAYER = player_list.pop(0)
     player_list.append(ACTIVE_PLAYER)
 
-
 def place_marble():
     global CURRENT_MARBLE
+
     STACK.insert(0, STACK.pop(-1))
     STACK.insert(0, STACK.pop(-1))
-    STACK.insert(-1, CURRENT_MARBLE)
+    STACK.append(CURRENT_MARBLE)
     CURRENT_MARBLE += 1
 
 
 def turn():
-    global CURRENT_MARBLE, BEST_MARBLE
+    global CURRENT_MARBLE
     player_change()
     if CURRENT_MARBLE % 23 == 0:
         # Score
-        marble = CURRENT_MARBLE
-        for X in range(0, 7):
-            STACK.insert(-1, STACK.pop(0))
-        removed = STACK.pop(-3)
-        SCORE[ACTIVE_PLAYER] += marble + removed
-        BEST_MARBLE = marble
+        for _ in range(0, 7):
+            STACK.append(STACK.pop(0))
+        SCORE[ACTIVE_PLAYER] += CURRENT_MARBLE + STACK.pop(-1)
         CURRENT_MARBLE += 1
         player_change()
-        STACK.insert(-4, CURRENT_MARBLE)
-        CURRENT_MARBLE += 1
-    else:
-        place_marble()
 
-print(game(9, 23))
+    place_marble()
+
+answer = game(405, 71700*100)
+
+print(max(zip(answer.values(), answer.keys())))
