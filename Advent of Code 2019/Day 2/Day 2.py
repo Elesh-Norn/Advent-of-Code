4d458b09e1f7addbfa94f0c24121a7bb15bf1b51
Elesh-Norn <kercyan@live.fr>
2019-12-02 19:34:09 +0100

    Day 2

--- Advent of Code 2019/Day 2/Day 2.py
+++ Advent of Code 2019/Day 2/Day 2.py
@@ -10,38 +10,31 @@ def read_input():
 
 def read_intcodes(intcodes):
     pointer = 0
-    loop = True
-    while loop:
+    while True:
+        one = intcodes[pointer + 1]
+        two = intcodes[pointer + 2]
+        three = intcodes[pointer + 3]
         if intcodes[pointer] == 99:
-            loop = False
+            return intcodes
 
         elif intcodes[pointer] == 1:
-            one = intcodes[pointer + 1]
-            two = intcodes[pointer + 2]
-            three = intcodes[pointer + 3]
-
             intcodes[three] = intcodes[one] + intcodes[two]
-        elif intcodes[pointer] == 2:
-            one = intcodes[pointer + 1]
-            two = intcodes[pointer + 2]
-            three = intcodes[pointer + 3]
 
+        elif intcodes[pointer] == 2:
             intcodes[three] = intcodes[one] * intcodes[two]
 
         else:
             print("Something went wrong!")
-            loop = False
+            return None
 
         pointer += 4
-
-    return intcodes
 
 
 # Part 1
 testcodes = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
 print(read_intcodes(testcodes))
 
-part1=read_input()
+part1 = read_input()
 part1[1] = 12
 part1[2] = 2
 print(read_intcodes(part1)[0])
@@ -55,21 +48,20 @@ def guess_the_noun_verb(guess):
         intcodes = read_input()
         intcodes[1] = noun
         intcodes[2] = verb
+
         if read_intcodes(intcodes)[0] == guess:
-            print(verb, noun)
-            print(verb + noun *100)
-            return True
+            return verb + noun *100
+
         noun += 1
         if noun > 99:
             noun = 0
             verb += 1
-        if verb > 99:
-            print("Big Problem")
-            return False
 
+        if verb > 99:
+            return None
 
 # Test Part 2
-guess_the_noun_verb(2692315)
+print(guess_the_noun_verb(2692315))
 
 # Part 2 answer
-guess_the_noun_verb(19690720)
+print(guess_the_noun_verb(19690720))
